479b8aa2ab780edc864971b0297c607faafcc74e
zz381435 <54127041+zz381435@users.noreply.github.com>
2020-12-18 19:43:20 +0800

    wz

---
+++ test/test01/src/constroller/servletIamge.java
@@ -0,0 +1,103 @@
+package constroller;
+
+import image.CreateImage;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.imageio.ImageIO;
+import javax.jms.Session;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/servlet/servletIamge")
+public class servletIamge extends HttpServlet {
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 创建一个验证码创建类
+		CreateImage createImage = new CreateImage();
+		// 产生四个字符串
+		String vCode = createImage.createCode();
+		// 获取session对象
+		HttpSession session  = request.getSession();
+		// 将生成的字符串放入session
+		session.setAttribute("vCode", vCode);
+		// 设置返回内容
+		response.setContentType("img/jpeg");
+		// 验证码刷新 需要取消缓存
+		response.setHeader("Pragma", "No-cache");
+		response.setHeader("Cache-Control", "no-cache");
+		// 设置失效时间
+		response.setDateHeader("Expires", 0);
+		// 以字节流发送
+		BufferedImage image = createImage.createCodeImage(vCode);
+		ServletOutputStream out = response.getOutputStream();
+		ImageIO.write(image, "JPEG", out);
+		out.flush();
+		out.close();
+//		System.out.println(111);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the POST method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
---
+++ test/test01/src/entity/User.java
@@ -0,0 +1,41 @@
+package entity;
+
+
+
+public class User {
+	// 用户真实名称
+	private String name;
+	// 用户密码
+	private String password;
+	// 用户名称
+	private String userName;
+	// 角色
+	private String role;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
